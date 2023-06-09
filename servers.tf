89a9ccea7ef05ef11b1f43072d0a229380a4b876
Sreeni.B <sreenu.reddy002@gmail.com>
2023-06-09 11:56:47 +0530

    For each loop implementation for roboshop

--- servers.tf
+++ servers.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "instance" {
   for_each = var.component
 #  count = length(var.component)
   ami           = data.aws_ami.centos.image_id
-  instance_type = each.value[instance_type]
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
